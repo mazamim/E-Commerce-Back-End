99e5e3fe8b6f99adec14643ed250e0f77f50cc3f
Mohammed Mazahim <mazamim91@gmail.com>
2022-03-01 00:41:16 +1100

    procts and category routes solved

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,36 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    price: {
+      type: DataTypes.DECIMAL(10, 2),
+      allowNull: true
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 0
+    },
+    category_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'category',
+        key: 'id',
+        unique: false
+      }
+    }
+ 
+
   },
   {
     sequelize,
